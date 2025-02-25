3edaf6a7fbe2497795bc71c8c07661c0a9142540
Caner Uruş <canerurus@outlook.com.tr>
2025-02-25 18:14:48 +0300

    Add project files.

---
+++ ApiProjecKampi.WebApi/Entities/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjecKampi.WebApi.Entities
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+        public string ImageURl { get; set; }
+    }
+}
